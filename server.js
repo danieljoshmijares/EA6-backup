64e67559425cad076b97d0db8839a006db74247d
Daniel Josh Abelarde Mijares <160100919+danieljoshmijares@users.noreply.github.com>
2025-07-25 23:39:26 +0000

    Latest server.js

--- server.js
+++ server.js
@@ -68,6 +68,6 @@ app.delete('/api/employees/:empid', async (req, res) => {
   }
 });
 
-// Start Server
+// Start Server 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
