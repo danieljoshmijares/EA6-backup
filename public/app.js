8bb318df4d72f13ec0a5e8add7df552d4c3db846
Daniel Josh Abelarde Mijares <160100919+danieljoshmijares@users.noreply.github.com>
2025-07-25 14:18:54 +0000

    Working server, not functioning correctly yet

--- public/app.js
+++ public/app.js
@@ -1,5 +1,5 @@
 // Base API URL - Matched with server port number
-const API_URL = 'http://localhost:2000/api/employees';
+const API_URL = 'https://glorious-space-cod-v6p6gw6qpj4xcw6q-3001.app.github.dev/api/employees';
 
 // DOM Elements
 const addForm = document.getElementById('addForm');
